231634687e041b1d9c8ba9f55b99a2011a4ca32a
Abdelmounaim-Abounore <abdelmounaim.abounore@gmail.com>
2023-12-18 16:00:48 +0100

    print the invoice

--- Frontend/src/components/Payment.jsx
+++ Frontend/src/components/Payment.jsx
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import Logo from '../assets/Picto_Blog_Assurance.png'
 
 const PaymentModal = ({ isOpen, onClose, appartement }) => {
     const [appartementId, setAppartementId] = useState(appartement._id);
+    const [appartementAddress, setappartementAddress] = useState(appartement.address);
     const [paiements, setPaiements] = useState([]);
 
     const getCookie = (name) => {
         const cookies = document.cookie.split(';').map(cookie => cookie.trim());
         const cookie = cookies.find(cookie => cookie.startsWith(`${name}=`));
         if (cookie) {
-          return cookie.split('=')[1];
+            return cookie.split('=')[1];
         }
         return null;
-      };
+    };
     const token = getCookie('jwtToken');
     const tokenWithdots = token.replace(/~/g, '.');
 
@@ -31,14 +35,46 @@ const PaymentModal = ({ isOpen, onClose, appartement }) => {
         displayPayment();
     }, []);
 
+    const downloadPaymentDetailsAsPDF = async (appartementAddress, paiement) => {
+        try {
+            const paymentElement = document.createElement('div');
+            paymentElement.innerHTML = `
+            <div class="m-5 mr-6 ml-5 flex flex-shrink-0 items-center text-blue-900">
+                <img src="${Logo}" alt="Logo" class="mt-4" style="width: 40px;" />
+                <span class="font-semibold text-xl tracking-tight">
+                    Castilla Syndic 
+                </span>
+            </div>
+            <h2 class="m-3 p-3">Payment Facture of <span class="text-blue-800">${appartementAddress}</span> Apartment</h2>
+            <p class="m-3 p-3">The month <span class="text-blue-800">${paiement.month}</span> of the year <span class="text-blue-800">${paiement.year}</span> has been paid</p>
+            <p class="text-blue-800 mt-3 m-3 p-3 text-xs">Castilla Syndic</p>
+            <br><br>
+          `;
+
+            document.body.appendChild(paymentElement);
+
+            const canvas = await html2canvas(paymentElement);
+            const imgData = canvas.toDataURL('image/png');
+
+            const pdf = new jsPDF();
+            pdf.addImage(imgData, 'PNG', 10, 10);
+
+            pdf.save('payment_details.pdf');
+
+            document.body.removeChild(paymentElement);
+        } catch (error) {
+            console.error('Failed to generate PDF', error);
+        }
+    };
+
     return (
         <div className={`modal ${isOpen ? 'open' : ''}`}>
             <div className="">
                 <div className="w-11/12 m-auto relative top-[-160px]">
                     <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t bg-gray-500	 dark:border-gray-600">
+                        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t bg-gray-400 dark:border-gray-600">
                             <h3 className="text-xl font-semibold text-gray-100 dark:text-white">
-                                Paiments
+                                Paiments of {appartementAddress} Appartement
                             </h3>
                             <button
                                 type="button"
@@ -68,8 +104,10 @@ const PaymentModal = ({ isOpen, onClose, appartement }) => {
                             {paiements.length > 0 ? (
                                 <div className="flex flex-wrap">
                                     {paiements.map((paiement, index) => (
-                                        <p key={index}
-                                            className={`w-1/5 p-5 flex-shrink-0 border border-gray-300 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-GRAY-200'}`}>
+                                        <p
+                                            key={index}
+                                            onClick={() => downloadPaymentDetailsAsPDF(appartementAddress, paiement)}
+                                            className={`w-1/5 p-5 flex-shrink-0 border border-gray-300 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-gray-200'} hover:bg-red-200 cursor-pointer`}>
                                             {paiement.month} - {paiement.year}
                                         </p>
                                     ))}
